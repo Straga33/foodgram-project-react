6aa1e98cd5f87eaf7d278f69e0bd5b853096e1a3
Michail Baskov <baem-festa@yandex.ru>
2022-10-05 02:14:07 +0300

    add_manage_commands

---
+++ backend/recipes/management/commands/loadadd_tags.py
@@ -0,0 +1,20 @@
+
+from django.core.management import BaseCommand
+
+from recipes.models import Tag
+
+
+class Command(BaseCommand):
+    help = 'Loads tags'
+
+    def handle(self, *args, **kwargs):
+        data = [
+            {'name': 'завтрак', 'color': '#008000', 'slug': 'breakfast'},
+            {"name": "обед", 'color': '#FF4500', 'slug': 'lunch'},
+            {'name': "ужин", 'color': '#00008B', 'slug': 'dinner'},
+            {'name': 'полдник', 'color': '#800080', 'slug': 'snack'}
+        ]
+        Tag.objects.bulk_create(Tag(**tag) for tag in data)
+        self.stdout.write(
+            self.style.SUCCESS("***Tags were succesfully loaded***")
+        )
